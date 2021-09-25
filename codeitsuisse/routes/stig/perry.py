df7918293a03a212412b5199b992cb39098ef57e
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2021-09-26 01:33:13 +0800

    test endpoint

--- codeitsuisse/routes/stig/perry.py
+++ codeitsuisse/routes/stig/perry.py
@@ -2,6 +2,7 @@ import logging
 import json
 
 from flask import request, jsonify
+from itertools import product
 
 from codeitsuisse import app
 
@@ -12,26 +13,25 @@ logger = logging.getLogger(__name__)
 def perry():
     interviews = request.get_json()
     logging.info("data sent for evaluation {}".format(interviews))
-    result = interpret_interviews(interviews)
+    # result = interpret_interviews(interviews)
 
-    logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    # logging.info("My result :{}".format(result))
+    return json.dumps(0)
 
+def all_repeat(rno):
+    results = []
+    for c in product([True, False], repeat = rno):
+        results.append(c)
+    return results
 
-def interpret_interviews(interviews: list) -> list:
+def interpret_interviews(interviews: list):
     interview_outputs = []
     for interview in interviews:
         max_rating = interview["maxRating"]
         questions = interview["questions"]
 
-        low = 1
-        high = max_rating
-
-        for question in questions:
-            interval = question[0]  # each question contains only one interval
-
-            low = max(low, interval["from"])
-            high = min(high, interval["to"])
+        sets = []
+        
 
         output = {p: 1, q: high - low + 1}
         interview_outputs.append(output)
