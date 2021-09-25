69a3802a8167cf9c869b2e4b5d724fc52f2b6e56
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2021-09-25 09:44:14 +0800

    exposing ttt endpoint

---
+++ codeitsuisse/routes/tictactoe.py
@@ -0,0 +1,18 @@
+import logging
+import json
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/tic-tac-toe', methods=['POST'])
+def evaluate():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    # logging.info("My result :{}".format(result))
+    return json.dumps(0)
+
+
+
