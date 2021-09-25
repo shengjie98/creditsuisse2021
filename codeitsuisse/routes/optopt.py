eb5bddf03e70e9ebbb14abe33f7df10719b749e2
zhaojie-kok <kok.zhaojie@gmail.com>
2021-09-25 15:49:39 +0800

    rnpope

--- codeitsuisse/routes/optopt.py
+++ codeitsuisse/routes/optopt.py
@@ -13,39 +13,49 @@ def optopt():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
 
-    return json.dumps(rngsus(**data))
+    return json.dumps(helplah(**data))
 
-# def helplah(options, views):
-#     expected_market_val = 0
-#     total_weight = 0
-#     for v in views:
-#         expected_market_val += v['mean'] * v['weight']
-#         total_weight += v['weight']
+def helplah(options, view):
+    expected_market_val = 0
+    total_weight = 0
+    for v in view:
+        expected_market_val += v['mean'] * v['weight']
+        total_weight += v['weight']
 
-#     expected_market_val /= total_weight
+    expected_market_val /= total_weight
 
-#     for o in options:
-#         if o['type'] == 'call':
-#             o['expectation'] = expected_market_val - o['strike'] - o['premium']
-#         else:
-#             o['expectation'] = expected_market_val
+    for o in options:
+        if o['type'] == 'call':
+            o['expectation'] = expected_market_val - o['strike'] - o['premium']
+        else:
+            o['expectation'] = o['strike'] + o['premium'] - expected_market_val
+
+    best = 0
+    for i in range(len(options)):
+        if options[i]['expectation'] > options[best]['expectation']:
+            best = i
+
+    shares = [0] * len(options)
+    shares[best] = 100
+
+    return shares
 
 
-def rngsus(options, view):
-    rng = [random() for i in range(len(options))]
-    total = sum(rng)
-    for i in range(len(rng)):
-        rng[i] = int(100 * rng[i]/total)
+# def rngsus(options, view):
+#     rng = [random() for i in range(len(options))]
+#     total = sum(rng)
+#     for i in range(len(rng)):
+#         rng[i] = int(100 * rng[i]/total)
     
-    total = sum(rng)
-    while total > 100:
-        randind = randrange(0, 100)
-        rng[randind] -= 1
-        total -= 1
+#     total = sum(rng)
+#     while total > 100:
+#         randind = randrange(0, 100)
+#         rng[randind] -= 1
+#         total -= 1
     
-    for i in range(len(rng)):
-        if options[i]['type'] == 'put':
-            rng[i] *= -1
+#     for i in range(len(rng)):
+#         if options[i]['type'] == 'put':
+#             rng[i] *= -1
 
-    return rng
+#     return rng
     
