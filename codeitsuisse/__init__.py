1530b8fff12c5c1ae47ab4e819991eb616683bf3
zhaojie-kok <kok.zhaojie@gmail.com>
2021-09-25 14:52:17 +0800

    walao sian

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 import codeitsuisse.routes.square
+import codeitsuisse.routes.optopt
 import codeitsuisse.routes.asteroid
 import codeitsuisse.routes.parasite
 import codeitsuisse.routes.tictactoe
