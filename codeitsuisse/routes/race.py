b4e0290b0c07eda2bfaf14795a12996610b40aeb
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2021-09-25 23:57:46 +0800

    wuhkf

--- codeitsuisse/routes/race.py
+++ codeitsuisse/routes/race.py
@@ -3,6 +3,7 @@ import json
 
 from flask import request, jsonify
 
+from collections import Counter
 from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
@@ -11,7 +12,17 @@ logger = logging.getLogger(__name__)
 def race():
     data = request.get_data(as_text=True)
     logging.info("data sent for evaluation {}".format(data))
-    return data
+    with open('results.txt') as f:
+        lines = f.readlines()
+    all_racers = []
+    for line in lines:
+        racers = line.lstrip('Your guess is ').split(', ')
+        racers[-1] = racers[-1].split('.')[0]
+        all_racers.extend(racers)
+    ctr = Counter(all_racers)
+    swimmers = data.split(',')
+    swimmers.sort(key=lambda x: ctr[x])     
+    return ','.join(swimmers)
 
 
 
