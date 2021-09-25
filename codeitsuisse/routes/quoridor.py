b5fa28cbce619963c4bef9466fc4c3e9bed434e1
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2021-09-25 17:51:26 +0800

    bullshit

---
+++ codeitsuisse/routes/quoridor.py
@@ -0,0 +1,39 @@
+import logging
+import json
+import requests
+import sseclient
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+url = "https://cis2021-arena.herokuapp.com/quoridor/start/"
+play_url = "https://cis2021-arena.herokuapp.com/quoridor/play/"
+
+@app.route('/quoridor', methods=['POST'])
+def get_id():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    battleId = data['battleId']
+    endpoint = url + battleId
+    play_end = play_url + battleId
+
+    stream_response = requests.get(endpoint, stream=True)
+    client = sseclient.SSEClient(stream_response)
+
+    to_post = {
+        "action": "(╯°□°)╯︵ ┻━┻"
+    }
+    x = requests.post(play_end, json = to_post)
+    logging.info(x.status_code, to_post)
+    # Loop forever (while connection "open")
+    # for i, event in enumerate(client.events()):
+    #     logging.info(event.data)
+    #     d = json.loads(event.data)
+    #     if i == 0:
+    #         # board = createBoard()   
+    #         player = d['youAre']
+    #         if player == 'second':
+    
