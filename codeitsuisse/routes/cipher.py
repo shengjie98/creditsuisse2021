a3460f410344c0cc866efc9089db1dc7cbbfe8fa
zhaojie-kok <kok.zhaojie@gmail.com>
2021-09-26 09:06:21 +0800

    sadf

--- codeitsuisse/routes/cipher.py
+++ codeitsuisse/routes/cipher.py
@@ -17,23 +17,23 @@ logger = logging.getLogger(__name__)
 def crack():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    # ans = [1] * len(data)
-    # hashes = {}
+    ans = [1] * len(data)
+    hashes = {}
 
-    # for i, d in enumerate(data):
-    #     if d['D'] <= 4:
-    #         hashes[d['Y']] = i
-    #     else:
-    #         break
+    for i, d in enumerate(data):
+        if d['D'] <= 4:
+            hashes[d['Y']] = i
+        else:
+            break
 
-    # for k in range(1, 10**4+1):
-    #     for f in range(1, 100001):
-    #         ind = hashes.get(sha256(f"{k}::{f/1000}".encode()).hexdigest())
-    #         if ind is not None:
-    #             ans[ind] = k
-    #             print(ans)
+    for k in range(1, 10**4+1):
+        for f in range(1, 100001):
+            ind = hashes.get(sha256(f"{k}::{f/1000}".encode()).hexdigest())
+            if ind is not None:
+                ans[ind] = k
+                print(ans)
         
-    return json.dumps([8, 4, 5, 9, 1, 4, 2, 6, 10, 8, 8, 10, 5, 10, 1, 9, 696, 6945, 6221, 4834, 5497, 5281, 1, 1, 1, 1, 1, 1, 1, 1])
+    return json.dumps(ans)
 
 
 def brute(d, y, f_floor):
