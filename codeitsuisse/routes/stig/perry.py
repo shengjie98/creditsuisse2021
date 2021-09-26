17ec6a6e91767bf9b9236af46ec863f87dafdb9e
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2021-09-26 09:03:31 +0800

    pls dont oom

--- codeitsuisse/routes/stig/perry.py
+++ codeitsuisse/routes/stig/perry.py
@@ -16,7 +16,9 @@ def perry():
     interviews = request.get_json()
     # logging.info("data sent for evaluation {}".format(interviews))
     # result = interpret_interviews(interviews)
-    result = dumb(interviews)
+    result = []
+    for test_case in interviews:
+        result.append(dumb(test_case))
 
     logging.info("My result :{}".format(result))
     return app.response_class(
@@ -25,39 +27,35 @@ def perry():
         mimetype='application/json'
     )
 
-def dumb(data):
-    res = []
-    for test_case in data:
-        logging.info(len(test_case['questions']))
-        arr = []
-        for i, q in enumerate(test_case['questions']):
-            arr.append((q[0]["from"], i, False))
-            arr.append((q[0]["to"], i, True))
-        arr.append((1, -1, False))
-        arr.append((1e9, -1, True))
-        arr.sort(key=lambda x: x[2])
-        arr.sort(key=lambda x: x[0])
-        S = set()
-        ans = set()
-        for i in range(1, len(arr)):
-            n, a, e = arr[i-1]
-            m, b, f = arr[i]
-            if e == False:
-                S.add(a)
-            else:
-                S.remove(a)
-            n_prime = n + 1 if e else n
-            m_prime = m if f else m - 1
-            if n_prime <= m_prime:
-                ans.add(','.join(map(str, sorted(tuple(S)))))
-        p = len(ans)
-        q = 1000000000
-        factor = gcd(p, q)
-        res.append({
-            "p": int(p//factor),
-            "q": int(q//factor)
-        })
-    return res
+def dumb(test_case):
+    arr = []
+    for i, q in enumerate(test_case['questions']):
+        arr.append((q[0]["from"], i, False))
+        arr.append((q[0]["to"], i, True))
+    arr.append((1, -1, False))
+    arr.append((1e9, -1, True))
+    arr.sort(key=lambda x: x[2])
+    arr.sort(key=lambda x: x[0])
+    S = set()
+    ans = set()
+    for i in range(1, len(arr)):
+        n, a, e = arr[i-1]
+        m, _, f = arr[i]
+        if e == False:
+            S.add(a)
+        else:
+            S.remove(a)
+        n_prime = n + 1 if e else n
+        m_prime = m if f else m - 1
+        if n_prime <= m_prime:
+            ans.add(','.join(map(str, sorted(tuple(S)))))
+    p = len(ans)
+    q = 1000000000
+    factor = gcd(p, q)
+    return {
+        "p": int(p//factor),
+        "q": int(q//factor)
+    }
 
 
 
