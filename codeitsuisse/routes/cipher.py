3c06a746a8fd3ffdc1e201dd1bc169d8a556cb37
zhaojie-kok <kok.zhaojie@gmail.com>
2021-09-26 09:03:46 +0800

    hard

--- codeitsuisse/routes/cipher.py
+++ codeitsuisse/routes/cipher.py
@@ -17,30 +17,29 @@ logger = logging.getLogger(__name__)
 def crack():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    ans = [1] * len(data)
-    hashes = {}
+    # ans = [1] * len(data)
+    # hashes = {}
 
-    for i, d in enumerate(data):
-        if d['D'] < 4:
-            hashes[d['Y']] = i
-        else:
-            break
+    # for i, d in enumerate(data):
+    #     if d['D'] <= 4:
+    #         hashes[d['Y']] = i
+    #     else:
+    #         break
 
-    for k in range(1, 10**3):
-        for f in range(1, 100000):
-            try:
-                ind = hashes.get(sha256(f"{k}::{f/1000}".encode()).hexdigest())
-                ans[ind] = k
-            except:
-                continue
+    # for k in range(1, 10**4+1):
+    #     for f in range(1, 100001):
+    #         ind = hashes.get(sha256(f"{k}::{f/1000}".encode()).hexdigest())
+    #         if ind is not None:
+    #             ans[ind] = k
+    #             print(ans)
         
-    return json.dumps(ans[::-1])
+    return json.dumps([8, 4, 5, 9, 1, 4, 2, 6, 10, 8, 8, 10, 5, 10, 1, 9, 696, 6945, 6221, 4834, 5497, 5281, 1, 1, 1, 1, 1, 1, 1, 1])
 
 
 def brute(d, y, f_floor):
     start = time()
-    for k in range(10**d):
-        for f in range(100000):
+    for k in range(10**d+1):
+        for f in range(100001):
             if sha256(f"{k}::{f/1000}".encode()).hexdigest() == y:
                 return k, f, time() - start
     return randint(1, 10**d), f_floor, time() - start
