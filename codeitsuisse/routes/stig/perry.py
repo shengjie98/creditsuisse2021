9d98ecc4e894cd0a069d6143326aa55d84845da7
shengjie98 <53373032+shengjie98@users.noreply.github.com>
2021-09-26 08:57:21 +0800

    oom

--- codeitsuisse/routes/stig/perry.py
+++ codeitsuisse/routes/stig/perry.py
@@ -38,7 +38,7 @@ def dumb(data):
         arr.sort(key=lambda x: x[2])
         arr.sort(key=lambda x: x[0])
         S = set()
-        ans = []
+        ans = set()
         for i in range(1, len(arr)):
             n, a, e = arr[i-1]
             m, b, f = arr[i]
@@ -49,11 +49,9 @@ def dumb(data):
             n_prime = n + 1 if e else n
             m_prime = m if f else m - 1
             if n_prime <= m_prime:
-                ans.append((n_prime, m_prime, tuple(sorted(tuple(S)))))
-        all_possible_sets = {n[2] for n in ans}
-        print(all_possible_sets)
-        p = len(all_possible_sets)
-        q = 1e9
+                ans.add(','.join(map(str, sorted(tuple(S)))))
+        p = len(ans)
+        q = 1000000000
         factor = gcd(p, q)
         res.append({
             "p": int(p//factor),
@@ -61,66 +59,5 @@ def dumb(data):
         })
     return res
 
-def gcd(a,b):
-    if a == 0:
-        return b
-    return gcd(b % a, a)
- 
-# Function to return LCM of two numbers
-def lcm(a,b):
-    return (a / gcd(a,b))* b
 
 
-
-def interpret_interviews(interviews: list):
-    interview_outputs = []
-    for interview in interviews:
-        max_rating = interview["maxRating"]
-        questions = interview["questions"]
-
-        D = initializeDiffArray()
-        for question in questions:
-            update(D, question[0]['from'], question[0]['to'])
-        A = scanArray(D)
-        print(A)
-        # interview_outputs.append(output)
-    return interview_outputs
-
-def initializeDiffArray():
-    
-
-    # We use one extra space because
-    # update(l, r, x) updates D[r+1]
-    # D = [0 for i in range(0 , n + 1)]
-
-    # D[0] = A[0]; D[n] = 0
-    # for i in range(1, n ):
-    #     D[i] = A[i] - A[i - 1]
-    return defaultdict(int)
-
-# Does range update
-def update(D, l, r):
-    D[l] += 1
-    D[r + 1] -= 1
-
-# Prints updated Array
-def scanArray(D):
-    A = defaultdict(int)
-    print(D)
-    for i, (key, val) in enumerate(sorted([(k, v) for k, v in D.items()])):
-        if i == 0:
-            A[i] = val
-        else:
-            A[i] = val + A[i-1]
-    for i in range(0 , len(A)):
-        if (i == 0):
-            A[i] = D[i]
-    return A
-    #     # Note that A[0] or D[0] decides
-    #     # values of rest of the elements.
-    #     else:
-    #         A[i] = D[i] + A[i - 1]
-
-    #     print(A[i], end = " ")
-        
-    # print ("")
